10cb1c5710d72ecde6ae83beb68e55ba777aab17
zeeshanmalik0110 <zeeshandevsinn@gmail.com>
2024-01-11 14:49:46 +0500

    starting dashboard screen working

---
+++ lib/view/admin/dashboard.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class DashBoardScreen extends StatefulWidget {
+  const DashBoardScreen({super.key});
+
+  @override
+  State<DashBoardScreen> createState() => _DashBoardScreenState();
+}
+
+class _DashBoardScreenState extends State<DashBoardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Drawer
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
