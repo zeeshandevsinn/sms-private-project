d7b23794d65087f3abf8dd60b5933da768e7dd5a
zeeshanmalik0110 <zeeshandevsinn@gmail.com>
2024-01-31 12:18:19 +0500

    push code work only something

---
+++ lib/view/admin/utils/custom_view_student_container.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:sms_application_flutter/utils/app_colors.dart';
+import 'package:sms_application_flutter/utils/image_widget.dart';
+import 'package:sms_application_flutter/utils/text-styles.dart';
+
+class CustomViewStudentContainer extends StatelessWidget {
+  CustomViewStudentContainer(
+      {super.key,
+      required this.title,
+      required this.subtitle,
+      required this.containerText});
+  var title = "", subtitle = "", containerText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Container(
+        width: 250,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          border: Border.all(color: Color.fromARGB(255, 179, 174, 174)),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              ListTile(
+                leading: ImageWidget(
+                  profileImage: 'assets/images/logo.jpg',
+                  isRadius: true,
+                ),
+                title: Text(
+                  title,
+                  style: AppTextStyles.textStyleBoldBodyXSmall,
+                ),
+                subtitle: Text(
+                  subtitle,
+                  style: AppTextStyles.textStyleBoldBodyXSmall,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(10)),
+                  Text(
+                    containerText,
+                    style: AppTextStyles.textStyleBoldBodyXSmall,
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                    color: Colors.black,
+                    size: 12,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
